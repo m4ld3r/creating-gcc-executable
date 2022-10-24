a91de2982734907568aba1677a213f1734610b83
Nick Malder <Banzai.Malder@protonmail.ch>
2022-10-25 01:12:29 +0300

    added support header files

--- gcc_cpp.sh
+++ gcc_cpp.sh
@@ -1,19 +1,33 @@
 #!/usr/bin/bash
-echo "Enter the PATH to the compiled file: "
-read way
-cowsay -f stimpy "I start fucking through..."
-sleep 2
-echo "3"
-sleep 2
-echo "2"
-sleep 2
-echo "1"
-sleep 2
-echo "Let's go. For watch result program press ctrl+z"
-sleep 2
-while :
-    do
-	g++ $way -lm -o ${way%%.cpp}
-	chmod +x ${way%%.cpp}
-	trap "./${way%%.cpp}" SIGTSTP
-    done
+
+echo -n "Enter the number of compilable items:"
+read ch
+
+if [ $ch -ne 1 ]
+then
+	echo "\nEnter all header files separated by a space:"
+	read -a mass
+	echo "\nEnter main file:"
+	read main
+	echo "\nEnter PATH to dir with all .o files:\n"
+	read PATH
+	cd $PATH
+	echo " "
+	while :
+	do
+		g++ -c ${mass[@]}
+		g++ -c $main
+		g++ *.o -lm -o ${main%%.cpp}
+	done
+else
+	echo "\nEnter the PATH to the compiled file: "
+	read way
+	echo "Let's go. For watch result program press ctrl+z"
+	sleep 2
+	while :
+	do
+		g++ $way -lm -o ${way%%.cpp}
+		chmod +x ${way%%.cpp}
+		trap "./${way%%.cpp}" SIGTSTP
+	done
+fi
